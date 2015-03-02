5b19272f1ac7747432afe78e821126e48dd2effe
Talamantez <robert.talamantez2@gmail.com>
2015-03-02 00:56:59 -0800

    initial

---
+++ connect.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var SerialPort = require("serialport").SerialPort;
+
+var serialPort = new SerialPort("/dev/ttyACM0", {
+  baudrate: 9600
+});
+
---
+++ open.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var SerialPort = require("serialport").SerialPort;
+
+var serialPort = new SerialPort("/dev/ttyACM0", {
+	baudrate: 9600,
+	
+});
+
+serialPort.on("open", function () {
+	console.log('open');
+	serialPort.on('data', function( data ){
+		console.log(''+ data);
+	}); 
+});
